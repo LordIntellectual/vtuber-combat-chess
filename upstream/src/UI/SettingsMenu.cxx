7c2a8d7e1f8a7c6ca7894a6477668fbdf8df9844
Lord Intellectual <LordIntellectual@users.noreply.github.com>
2026-08-07 15:11:10 +1000

    ui: menu pulse size 10%; default music volume 60%

--- upstream/src/UI/SettingsMenu.cxx
+++ upstream/src/UI/SettingsMenu.cxx
@@ -22,7 +22,7 @@ SettingsMenu::SettingsMenu()
     quitYesX(0), quitYesY(0), quitYesW(140), quitYesH(40),
     quitNoX(0), quitNoY(0), quitNoW(140), quitNoH(40),
     editSlider(-1), editLen(0), editBlink(0.f),
-    master(0.9f), music(0.45f), sfx(0.7f), menuPulseSens(0.4f), outline(0.15f),
+    master(0.9f), music(0.60f), sfx(0.7f), menuPulseSens(0.4f), outline(0.15f),
     // White = red, Black = purple (project palette / multiplayer readability)
     outWhiteR(0.95f), outWhiteG(0.12f), outWhiteB(0.12f),
     outBlackR(0.62f), outBlackG(0.18f), outBlackB(0.92f),
